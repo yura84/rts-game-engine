c47898c925a680e26f506bff9f7e3ad172061839
yuramysan@gmail.com <yuramysan@gmail.com@gmail.com>
2012-12-13 01:26:45 +0200

    das

--- Branch/Editor/main.cpp
+++ Branch/Editor/main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <windows.h>
+#include <gl\gl.h>
+#include <gl\glu.h>
+#include <gl\glaux.h>
 
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-  return 0;
+
+    return 0;
 }
